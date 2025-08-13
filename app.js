23847b71942cc502f010aa868b788ee91fb23b46
estebanhfm <150342532+estebanhfm@users.noreply.github.com>
2025-08-12 22:51:42 -0400

    actualización
    Se agrega código para mostrar lista de nombres.

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-// A una array de amigos vacia se agregaran los amigos pulsando el boton añadir.
+
+// A una array de amigos vacia se agregaran los amigos pulsando el boton añadir. Alerta si el campo esta vacio.
 
     let amigos =[];
-    let ingresos = 0; 
-    
+        
 function agregarAmigo () {
-    amigos.push(document.getElementById('amigo').value);
-ingresos++;
+    
+if(document.getElementById('amigo').value== '') {
+    alert('debe agregar a un amigo') ;
+}else{
+   amigos.push(document.getElementById('amigo').value);
+    document.querySelector('#amigo').value = '';
+    mostrarAmigo();
+}
     console.log(amigos);
-    console.log(ingresos);
+  }
+
+// Crear lista de amigos participantes
+
+function mostrarAmigo () {
+    let lista = document.getElementById("listaAmigos"); // linea 27 html
+    lista.innerHTML = ''; // limpiar lista
+
+for (let i = 0; i < amigos.length; i++) {
+
+      let elementoLista = document.createElement('li');
+    elementoLista.textContent = amigos[i];
+    lista.appendChild(elementoLista);
+    }
+ 
 }
 
-// Al agregar un amigo con el boton añadiir, 
-// éste se almacena en un parrafo bajo el boton como participante del sorteo. 
-// se almacenan los indices del array o elementos de acuerdo al ingreso que lleva un contador.
 
-/* aun no se me ocurre como agregar la lista 
-
-let ingresos = 0;
-
-function asignarParticipante (elemento, texto) {
-    let participanteHTLM = document.createElement("p");
-    participanteHTLM.textContent = amigos[ingresos];
-
-}
-
-*/
-
-// al sortear amigo se aplicara un random a los numeros de ingresos 
-// de los elementos del array amigos
-// se mostrara el alert (tu amigo es , anotalo para que no se te olvide)
 
